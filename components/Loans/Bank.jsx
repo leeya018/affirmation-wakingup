bb87c424c0143557a34412c2ddb166065da50be0
leeya018 <leeyahav018@gmail.com>
2024-03-08 01:43:22 +0200

    e

--- components/Loans/Bank.jsx
+++ components/Loans/Bank.jsx
@@ -11,6 +11,8 @@ function BankLoan() {
   const [totalToBank, setTotalToBank] = useState(-1)
   const [totalToMe, setTotalToMe] = useState(-1)
   const [roiRatio, setRoiRatio] = useState(-1)
+  //
+  const [prints, setPrints] = useState([])
 
   useEffect(() => {
     if (money && monthsToRet && moneyToRetPerMonth) {
@@ -51,50 +53,93 @@ function BankLoan() {
       setRoiRatio(moneyShifting / money)
     }
   }, [money, monthsToRet, moneyToRetPerMonth])
+
+  console.log(prints)
+  const print = (data) => {
+    setPrints((prev) => [...prev, data])
+  }
   return (
-    <div className="flex items-center gap-2">
-      <div className="flex flex-col">
-        <span>loaner</span>
-        <div>
-          Bank - not accurate <br /> (showing better results for me)
+    <div>
+      {/* calc */}
+      <div className="flex items-center gap-2">
+        <div className="flex flex-col">
+          <span>loaner</span>
+          <div>
+            Bank - not accurate <br /> (showing better results for me)
+          </div>
         </div>
+        <div className="flex flex-col">
+          <span>money</span>
+          <input
+            type="number"
+            value={money}
+            onChange={(e) => setMoney(parseInt(e.target.value))}
+          />
+        </div>
+        <div className="flex flex-col">
+          <span>months to return</span>
+          <input
+            min={24}
+            type="number"
+            value={monthsToRet}
+            onChange={(e) => setMonthsToRet(parseInt(e.target.value))}
+          />
+        </div>
+        <div className="flex flex-col">
+          <span>return the bank per month</span>
+          <input
+            type="number"
+            value={moneyToRetPerMonth}
+            onChange={(e) => setMoneyToRetPerMonth(parseInt(e.target.value))}
+          />
+        </div>
+        <div className="flex flex-col">
+          <span>total for bank</span>
+          <span>{totalToBank.toFixed(0)}</span>
+        </div>
+        <div className="flex flex-col ">
+          <span className="bg-green">total for me</span>
+          <span>{totalToMe.toFixed(0)}</span>
+        </div>
+        <div className="flex flex-col">
+          <span>ROI ratio</span>
+          <span>{roiRatio.toFixed(0)}</span>
+        </div>
+        <button
+          className="p-2 bg-blue rounded-lg hover:bg-blueL_bank 
+      cursor-pointer"
+          onClick={() => print({ money: totalToMe, years: monthsToRet / 12 })}
+        >
+          print
+        </button>
       </div>
-      <div className="flex flex-col">
-        <span>money</span>
-        <input
-          type="number"
-          value={money}
-          onChange={(e) => setMoney(parseInt(e.target.value))}
-        />
-      </div>
-      <div className="flex flex-col">
-        <span>months to return</span>
-        <input
-          min={24}
-          type="number"
-          value={monthsToRet}
-          onChange={(e) => setMonthsToRet(parseInt(e.target.value))}
-        />
-      </div>
-      <div className="flex flex-col">
-        <span>return the bank per month</span>
-        <input
-          type="number"
-          value={moneyToRetPerMonth}
-          onChange={(e) => setMoneyToRetPerMonth(parseInt(e.target.value))}
-        />
-      </div>
-      <div className="flex flex-col">
-        <span>total for bank</span>
-        <span>{totalToBank.toFixed(0)}</span>
-      </div>
-      <div className="flex flex-col ">
-        <span className="bg-green">total for me</span>
-        <span>{totalToMe.toFixed(0)}</span>
-      </div>
-      <div className="flex flex-col">
-        <span>ROI ratio</span>
-        <span>{roiRatio.toFixed(0)}</span>
+      {/* prints */}
+      <div className="mt-10">
+        <ul className="flex flex-col gap-2">
+          {prints.map((print, key) => (
+            <li key={key}>
+              <div>
+                <span className="text-xl font-semibold">
+                  {print.years.toFixed(0)} years val {print.money.toFixed(0)}{" "}
+                  NIS
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+        {prints.length > 0 && (
+          <div className="bg-green">
+            <span className="text-xl font-semibold">
+              {prints.reduce(
+                (maxYear, print) => Math.max(maxYear, print.years),
+                -1
+              )}{" "}
+              years val{" "}
+              {prints.reduce((acc, print) => parseInt(print.money) + acc, 0)}{" "}
+              NIS
+            </span>
+          </div>
+        )}
       </div>
     </div>
   )
--- components/Loans/Private.jsx
+++ components/Loans/Private.jsx
@@ -10,6 +10,8 @@ function PrivateLoan() {
   const [totalToCust, setTotalToCust] = useState(-1)
   const [totalToMe, setTotalToMe] = useState(-1)
   const [roiRatio, setRoiRatio] = useState(-1)
+  //
+  const [prints, setPrints] = useState([])
 
   useEffect(() => {
     if (money && yearsToRet && intrPromToCust) {
@@ -30,47 +32,92 @@ function PrivateLoan() {
       setRoiRatio(totalToMeTmp / money)
     }
   }, [money, yearsToRet, intrPromToCust])
+
+  const print = (data) => {
+    setPrints((prev) => [...prev, data])
+  }
+
   return (
-    <div className="flex items-center gap-2">
-      <div className="flex flex-col">
-        <span>loaner</span>
-        <div>private</div>
-      </div>
-      <div className="flex flex-col">
-        <span>money</span>
-        <input
-          type="number"
-          value={money}
-          onChange={(e) => setMoney(parseInt(e.target.value))}
-        />
-      </div>
-      <div className="flex flex-col">
-        <span>years to return</span>
-        <input
-          type="number"
-          value={yearsToRet}
-          onChange={(e) => setYearsToRet(parseInt(e.target.value))}
-        />
-      </div>
-      <div className="flex flex-col">
-        <span>intrest promise to customer</span>
-        <input
-          type="number"
-          value={intrPromToCust}
-          onChange={(e) => setIntrPromToCust(parseInt(e.target.value))}
-        />
-      </div>
-      <div className="flex flex-col">
-        <span>total for customer</span>
-        <span>{totalToCust.toFixed(0)}</span>
-      </div>
-      <div className="flex flex-col ">
-        <span className="bg-green">total for me</span>
-        <span>{totalToMe.toFixed(0)}</span>
-      </div>
-      <div className="flex flex-col">
-        <span>ROI ratio</span>
-        <span>{roiRatio.toFixed(0)}</span>
+    <div>
+      <div>
+        <div className="flex items-center gap-2">
+          <div className="flex flex-col">
+            <span>loaner</span>
+            <div>private</div>
+          </div>
+          <div className="flex flex-col">
+            <span>money</span>
+            <input
+              type="number"
+              value={money}
+              onChange={(e) => setMoney(parseInt(e.target.value))}
+            />
+          </div>
+          <div className="flex flex-col">
+            <span>years to return</span>
+            <input
+              type="number"
+              value={yearsToRet}
+              onChange={(e) => setYearsToRet(parseInt(e.target.value))}
+            />
+          </div>
+          <div className="flex flex-col">
+            <span>intrest promise to customer</span>
+            <input
+              type="number"
+              value={intrPromToCust}
+              onChange={(e) => setIntrPromToCust(parseInt(e.target.value))}
+            />
+          </div>
+          <div className="flex flex-col">
+            <span>total for customer</span>
+            <span>{totalToCust.toFixed(0)}</span>
+          </div>
+          <div className="flex flex-col ">
+            <span className="bg-green">total for me</span>
+            <span>{totalToMe.toFixed(0)}</span>
+          </div>
+          <div className="flex flex-col">
+            <span>ROI ratio</span>
+            <span>{roiRatio.toFixed(0)}</span>
+          </div>
+          <button
+            className="p-2 bg-blue rounded-lg hover:bg-blueL_bank 
+      cursor-pointer"
+            onClick={() => print({ money: totalToMe, years: yearsToRet })}
+          >
+            print
+          </button>
+        </div>
+        {/* prints */}
+
+        <div className="mt-10">
+          <ul className="flex flex-col gap-2">
+            {prints.map((print, key) => (
+              <li key={key}>
+                <div>
+                  <span className="text-xl font-semibold">
+                    {print.years.toFixed(0)} years val {print.money.toFixed(0)}{" "}
+                    NIS
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+          {prints.length > 0 && (
+            <div className="bg-green">
+              <span className="text-xl font-semibold">
+                {prints.reduce(
+                  (maxYear, print) => Math.max(maxYear, print.years),
+                  -1
+                )}{" "}
+                years val{" "}
+                {prints.reduce((acc, print) => parseInt(print.money) + acc, 0)}{" "}
+                NIS
+              </span>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
